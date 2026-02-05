29d5854351e6bd61070caafdadc79b6b43e8474e
RezerOfgit <qingcaicoke@163.com>
2026-02-05 20:11:56 +0800

    feat:smallchange

---
+++ projects/src/com/rezero/smallchange/oop/SmallChangeSysAPP.java
@@ -0,0 +1,10 @@
+package com.rezero.smallchange.oop;
+
+/**
+ *  这里直接调用SmallChangeSysOOP 对象，显示主菜单即可
+ */
+public class SmallChangeSysAPP {
+    public static void main(String[] args) {
+        new SmallChangeSysOOP().mainMenu();
+    }
+}
