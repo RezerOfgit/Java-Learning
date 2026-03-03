dddc57bf94c9c72d3cee2d39537347f2cf431fa2
RezerOfgit <qingcaicoke@163.com>
2026-03-03 23:48:30 +0800

    feat: complete UDP broadcast and receiver implementations

--- chapters/chapter21/src/com/rezero/udp/UDPReceiverA.java
+++ chapters/chapter21/src/com/rezero/udp/UDPReceiverA.java
@@ -3,12 +3,14 @@ package com.rezero.udp;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /**
  * @author Re-zero
  * @version 1.0
  * UDP接收端
  */
+@SuppressWarnings({"all"})
 public class UDPReceiverA {
     public static void main(String[] args) throws IOException {
         //1. 创建一个 DatagramSocket 对象，准备在9999接收数据
@@ -17,7 +19,7 @@ public class UDPReceiverA {
         //2. 构建一个 DatagramPacket 对象，准备接收数据
         //在前面讲解UDP 协议时，老师说过一个数据包最大 64k
         byte[] buf = new byte[1024];
-        final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length);
 
         //3. 调用 接收方法，将通过网络传输的 DatagramPacket 对象
         //填充到 packet对象
@@ -31,6 +33,14 @@ public class UDPReceiverA {
         byte[] data = packet.getData();//接收到数据
         final String s = new String(data, 0, length);
         System.out.println(s);
+
+        //  回复信息给B端
+        // 将需要发送的数据，封装到 DatagramPacket对象
+        data = "好的, 明天见".getBytes();
+
+        //说明：封装的 DatagramPacket对象 data 内容字节数组,data.length,主机(IP),端口
+        packet = new DatagramPacket(data, data.length, InetAddress.getByName("192.168.91.105"), 9998);
+        socket.send(packet);//发送
 
         //5. 关闭资源
         socket.close();
