4b063428c10da5cb4f180e6a82dc5c459e84f117
RezerOfgit <qingcaicoke@163.com>
2026-03-19 17:11:30 +0800

    chore: add homework01.sql with SQL quiz answers

---
+++ mysql_notes/video_codes/homework01.sql
@@ -0,0 +1,29 @@
+-- 1.选择题homework01.sql
+-- （1）.以下哪条语句是错误的？
+-- A.SELECT empno,ename name,sal salary FROM emp;
+-- B. SELECT empno,ename name,sal AS salary FROM emp;
+-- C.SELECT ename,sal*12 AS "Annual Salary" FROM emp;
+-- D. SELECT ename,sal*12 Annual Salary FROM emp;
+
+-- D错误
+
+-- （2）.某用户希望显示补助非空的所有雇员信息，应该使用哪条语句？
+-- A.SELECT ename,sal,comm FROM emp WHERE comm<>null;
+-- B.SELECT ename,sal,comm FROM emp WHERE comm IS NOT null;
+-- C.SELECT ename,sal,comm FROM emp WHERE comm<>O;
+
+-- B
+
+-- 3）.以下哪条语句是错误的？
+-- A.SELECT ename,sal salary FROM emp ORDER BY sal;
+-- B. SELECT ename,sal salary FROM emp ORDER BY salary:
+-- C.SELECT ename,sal salary FROM emp ORDER BY 3;
+
+-- C
+
+
+
+
+
+
+
