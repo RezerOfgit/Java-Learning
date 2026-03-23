c4945b4378acd44ece6e16ddbec943d27bc762be
RezerOfgit <qingcaicoke@163.com>
2026-03-23 23:48:08 +0800

    feat(db): add account and admin2 table scripts with test data

--- mysql_notes/video_codes/jdbc_actor.sql
+++ mysql_notes/video_codes/jdbc_actor.sql
@@ -12,3 +12,53 @@ INSERT INTO ACTOR
 		VALUES (NULL ,'jack', '男', '1990-11-11', '112');
 INSERT INTO ACTOR 
 		VALUES (NULL ,'刘德华', '男', '1988-11-11', '110');
+
+CREATE TABLE account(
+		id int PRIMARY KEY AUTO_INCREMENT ,
+		name varchar(32) NOT NULL DEFAULT '',
+		balance double NOT NULL DEFAULT 0) CHARACTER SET utf8;
+
+INSERT INTO account
+		VALUES (NULL, '马云', 3000);
+INSERT INTO account
+		VALUES (NULL, '马化腾', 10000);
+
+SELECT * FROM account;
+
+-- 创建测试表
+CREATE TABLE admin2(
+		id int PRIMARY KEY AUTO_INCREMENT ,
+		username varchar(32) NOT NULL,
+		password varchar(32) NOT NULL );
+SELECT COUNT(*) FROM admin2;
+DROP TABLE admin2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
