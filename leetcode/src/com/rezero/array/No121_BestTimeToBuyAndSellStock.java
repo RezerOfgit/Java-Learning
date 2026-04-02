985d9f108c1409b07ddb13e74a2d9ff33caf0536
RezerOfgit <qingcaicoke@163.com>
2026-04-02 23:30:32 +0800

    feat(array): add optimal O(N) prefix-sum with hashmap solution for No.560 Subarray Sum Equals K

--- leetcode/src/com/rezero/array/No121_BestTimeToBuyAndSellStock.java
+++ leetcode/src/com/rezero/array/No121_BestTimeToBuyAndSellStock.java
@@ -3,8 +3,44 @@ package com.rezero.array;
 /**
  * @author Re-zero
  * @version 1.0
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。
+ * 设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
  */
 public class No121_BestTimeToBuyAndSellStock {
+
+    public int maxProfit_aiModify(int[] prices) {
+        int maxProfit = 0;
+        // 准备一个小本本，记录遍历过程中遇到的最低价格
+        // 初始设为一个极其大的数字，保证第一天的价格一定能写进本子里
+        int minPrice = Integer.MAX_VALUE;
+
+        // 只用一层循环！遍历每一天的价格
+        for (int i = 0; i < prices.length; i++) {
+            // 1. 如果今天的价格比历史最低价还要低，赶紧更新历史最低价
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            // 2. 如果今天不是最低价，那我就算算：如果今天卖出，能赚多少钱？
+            // 并把这个赚的钱和之前的 maxProfit 打擂台
+            else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        return maxProfit;
+
+    }
+    public int maxProfit_my(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+            }
+        }
+        return maxProfit;//暴力破解,但是超时了
+    }
 
     public int maxProfit(int[] prices) {
         // 1. 记录历史最低价，一开始设为一个极大的数（方便第一天直接更新）
---
+++ leetcode/src/com/rezero/hashmap/No560_SubarraySum.java
@@ -0,0 +1,40 @@
+package com.rezero.hashmap;
+
+import java.util.HashMap;
+
+/**
+ * @author Re-zero
+ * @version 1.0
+ * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+ * 子数组是数组中元素的连续非空序列。
+ */
+
+public class No560_SubarraySum {
+    public int subarraySum(int[] nums, int k) {
+        // map 记录：<前缀和, 该前缀和出现的次数>
+        HashMap<Integer, Integer> preSumMap = new HashMap<>();
+
+        // base case：前缀和为 0 的情况，默认出现了 1 次（代表什么都不选的时候和为 0）
+        preSumMap.put(0, 1);
+
+        int count = 0; // 记录满足条件的子数组个数
+        int preSum = 0; // 记录当前的累计前缀和
+
+        for (int num : nums) {
+            // 1. 累计当前的前缀和
+            preSum += num;
+
+            // 2. 核心逻辑：检查 HashMap 中是否曾经出现过 preSum - k
+            int target = preSum - k;
+            if (preSumMap.containsKey(target)) {
+                // 如果出现过，说明找到了一段和为 k 的子数组，累加它的出现次数
+                count += preSumMap.getOrDefault(target, 0);
+            }
+
+            // 3. 把当前的前缀和 preSum 存入 map 中（如果已存在则次数 +1，不存在则记为 1）
+            preSumMap.put(preSum, preSumMap.getOrDefault(preSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
