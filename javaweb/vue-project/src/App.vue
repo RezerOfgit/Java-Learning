857aad6f201810bed4f12195a4d81a5af65b9c3b
RezerOfgit <qingcaicoke@163.com>
2026-04-05 22:10:23 +0800

    fix(vue-router): resolve menu navigation failure and enable router mode
    - Enable 'router' attribute on el-menu for automatic path resolution.
    - Fix broken navigation links in EmpView by replacing plain text with route targets.
    - Prepare production build output via 'npm run build'.

--- javaweb/vue-project/src/App.vue
+++ javaweb/vue-project/src/App.vue
@@ -2,15 +2,17 @@
   <div>
 <!--    <element-view></element-view>-->
 <!--    员工管理-->
-    <emp-view></emp-view>
+<!--    <emp-view></emp-view>-->
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // import ElementView from "@/views/element/ElementView.vue";
-import EmpView from "@/views/tlias/EmpView.vue";
+// import EmpView from "@/views/tlias/EmpView.vue";
 export default {
-  components: { EmpView /*ElementView*/},
+  components: { /*EmpView*/ /*ElementView*/},
   data() { /* 简写, data: function () {*/
     return{
       message:"Hello Vue666"
--- javaweb/vue-project/src/views/tlias/EmpView.vue
+++ javaweb/vue-project/src/views/tlias/EmpView.vue
@@ -4,14 +4,15 @@
       <el-header style="font-size:40px; background-color: rgb(238, 241, 246)">tlias 智能学习辅助系统</el-header>
       <el-container>
         <el-aside width="230px" style="border: 1px solid #eee">
-          <el-menu :default-openeds="['1', '3']">
+          <el-menu :default-openeds="['1', '3']" router>
             <el-submenu index="1">
               <template slot="title"><i class="el-icon-message"></i>系统信息管理</template>
-              <el-menu-item index="1-1">部门管理</el-menu-item>
-              <el-menu-item index="1-2">员工管理</el-menu-item>
+              <el-menu-item index="/dept">部门管理</el-menu-item>
+              <el-menu-item index="/emp">员工管理</el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>
+
 
         <el-main>
           <!-- 表单 -->
@@ -53,13 +54,13 @@
             </el-table-column>
             <el-table-column label="性别" width="140">
               <template slot-scope="scope">
-                {{scope.row.gender == 1 ? '男':'女'}}
+                {{ scope.row.gender == 1 ? '男' : '女' }}
               </template>
             </el-table-column>
             <el-table-column prop="job" label="职位" width="140"></el-table-column>
             <el-table-column prop="entrydate" label="入职日期" width="180"></el-table-column>
             <el-table-column prop="updatetime" label="最后操作时间" width="230"></el-table-column>
-            <el-table-column label="操作" >
+            <el-table-column label="操作">
               <el-button type="primary" size="mini">编辑</el-button>
               <el-button type="danger" size="mini">删除</el-button>
             </el-table-column>
@@ -73,14 +74,12 @@
                          @size-change="handleSizeChange"
                          @current-change="handleCurrentChange"
                          :total="1000"></el-pagination>
+
         </el-main>
       </el-container>
     </el-container>
-
-
   </div>
 </template>
-
 
 <script>
 import axios from 'axios';
@@ -90,34 +89,32 @@ export default {
     return {
       tableData: [],
       searchForm: {
-        name:"",
-        gender:"",
-        entrydate:[]
+        name: "",
+        gender: "",
+        entrydate: []
       }
     }
   },
   methods: {
-    onSubmit:function(){
+    onSubmit: function () {
       alert("查询数据");
     },
-    handleSizeChange:function(val){
+    handleSizeChange: function (val) {
       alert("每页记录数变化" + val)
     },
-    handleCurrentChange:function(val){
+    handleCurrentChange: function (val) {
       alert("页码发生变化" + val)
     }
   },
-  mounted () {
+  mounted() {
     //发送异步请求,获取数据
-    axios.get("http://yapi.smart-xwork.cn/mock/169327/emp/list").then((result) => {
+    axios.get("https://mock.apifox.cn/m1/3128855-0-default/emp/list").then((result) => {
       this.tableData = result.data.data;
     });
   }
 }
-
 </script>
 
 <style>
-
 
 </style>
