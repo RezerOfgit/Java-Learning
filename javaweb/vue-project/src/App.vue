61495d41a5c8fffdc80dc8e53a0937bfb7df95de
RezerOfgit <qingcaicoke@163.com>
2026-04-05 20:56:34 +0800

    feat(vue): initialize standard Vue CLI project and integrate Element UI
    - Generate modern frontend engineering structure using Vue CLI.
    - Configure component routing and view directories (views/element, views/tlias).
    - Integrate Element UI library for rapid component-based development.
    - Ensure node_modules is properly excluded via .gitignore.

---
+++ javaweb/vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<!--    <element-view></element-view>-->
+<!--    员工管理-->
+    <emp-view></emp-view>
+  </div>
+</template>
+
+<script>
+// import ElementView from "@/views/element/ElementView.vue";
+import EmpView from "@/views/tlias/EmpView.vue";
+export default {
+  components: { EmpView /*ElementView*/},
+  data() { /* 简写, data: function () {*/
+    return{
+      message:"Hello Vue666"
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
