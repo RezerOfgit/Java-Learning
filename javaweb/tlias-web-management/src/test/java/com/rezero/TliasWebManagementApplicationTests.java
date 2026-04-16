278e5dab184b067c0c0da41163d316d76c85abe4
RezerOfgit <qingcaicoke@163.com>
2026-04-16 23:41:01 +0800

    feat: add JWT token generation and validation for login authentication

--- javaweb/tlias-web-management/src/test/java/com/rezero/TliasWebManagementApplicationTests.java
+++ javaweb/tlias-web-management/src/test/java/com/rezero/TliasWebManagementApplicationTests.java
@@ -1,11 +1,16 @@
 package com.rezero;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
-@SpringBootTest
+//@SpringBootTest
 class TliasWebManagementApplicationTests {
 
     @Test
@@ -18,5 +23,34 @@ class TliasWebManagementApplicationTests {
             String uuid = UUID.randomUUID().toString();
             System.out.println(uuid);
         }
+    }
+
+    /**
+     * 生成JWT
+     */
+    @Test
+    public void testGenJwt(){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id",1);
+        claims.put("name","tom");
+
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, "rezero")//签名算法
+                .setClaims(claims) //自定义内容(载荷)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))//设置有效期为1h
+                .compact();
+        System.out.println(jwt);
+    }
+
+    /**
+     * 解析JWT
+     */
+    @Test
+    public void testParseJwt(){
+        Claims claims = Jwts.parser()
+                .setSigningKey("rezero")
+                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidG9tIiwiaWQiOjEsImV4cCI6MTc3NjM1NzM1N30.nDdMm7a6k5l8EdC3DkPnkDINo3gJ5rwV3Ojvp_h4d2o")
+                .getBody();
+        System.out.println(claims);
     }
 }
