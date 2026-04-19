2e3c71b7b8141bff57bf5c3769bd50be25e8ff03
RezerOfgit <qingcaicoke@163.com>
2026-04-19 20:30:49 +0800

    feat: integrate custom utils module and update configuration

--- javaweb/day14_html/rezero-utils/src/main/java/com/rezeroutils/HeaderConfig.java
+++ javaweb/day14_html/rezero-utils/src/main/java/com/rezeroutils/HeaderConfig.java
@@ -1,5 +1,6 @@
 package com.rezeroutils;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class HeaderConfig {
 
     @Bean
+//    @ConditionalOnClass(name = "io.jsonwebtoken.Jwts") //环境中存在指定的这个类, 才会将该bean加入IOC容器中
+//    @ConditionalOnMissingBean //不存在该类型的bean，才会将该bean加入IOC容器中 --- 指定类型(value属性) 或 名称(name属性)
+    @ConditionalOnProperty(name = "name", havingValue = "rezero") //配置文件中存在指定的属性与值，才会将该bean加入IOC容器中
     public HeaderParser headerParser(){
         return new HeaderParser();
     }
