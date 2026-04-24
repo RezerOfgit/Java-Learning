5a0c2e0897eb7d89acd7c7ee0765b139b0965e38
RezerOfgit <qingcaicoke@163.com>
2026-04-24 16:44:55 +0800

    feat: add RedisTemplate quickstart demo

---
+++ redis/redis-demo/src/test/java/com/rezero/RedisDemoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.rezero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void testString() {
+        //插入一条string类型数据
+        redisTemplate.opsForValue().set("name", "李四");
+        //读取一条string类型数据
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
+    }
+
+}
