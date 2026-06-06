7daa0e3ec53e514b813940bfb84ef1416c3a5e14
RezerOfgit <qingcaicoke@163.com>
2026-06-06 22:31:03 +0800

    feat: add LeetCode 300 - Longest Increasing Subsequence

---
+++ coding-practice/handwritten-code/leetcode/Backtracking · DP · Greedy/No300_LongestIncreasingSubsequence.java
@@ -0,0 +1,29 @@
+/**
+ * LeetCode 300. 最长递增子序列
+ * 给你一个整数数组 nums，找到其中最长严格递增子序列的长度。
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
+ * 例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ */
+public class No300_LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // dp[i] 表示以 nums[i] 结尾的最长递增子序列的长度
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1); // 每个元素自身构成长度为 1 的子序列
+        int maxLen = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+}
