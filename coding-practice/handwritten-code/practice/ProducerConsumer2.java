2bfbc22e7bae327b06febcb0d42815a38bc3af6a
RezerOfgit <qingcaicoke@163.com>
2026-06-25 23:29:04 +0800

    feat: implement producer-consumer using BlockingQueue

---
+++ coding-practice/handwritten-code/practice/ProducerConsumer2.java
@@ -0,0 +1,26 @@
+public class ProducerConsumer2 {
+    private static final BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
+    public static void main(String[] args) {
+        Thread producer = new Thread(() -> {
+            while (true) {
+                try {
+                    queue.put(1);
+                    System.out.println("生产，当前库存：" + queue.size());
+                } catch (InterruptedException e) { break; }
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                try {
+                    queue.take();
+                    System.out.println("消费，当前库存：" + queue.size());
+                } catch (InterruptedException e) { break; }
+            }
+        });
+
+        producer.start();
+        consumer.start();
+    }
+}
