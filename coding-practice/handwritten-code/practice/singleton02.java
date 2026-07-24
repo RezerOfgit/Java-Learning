74558001295ad48fca222dd1510eff21dd4e029d
RezerOfgit <qingcaicoke@163.com>
2026-07-24 23:27:44 +0800

    feat(singleton): implement singleton using initialization-on-demand holder idiom

---
+++ coding-practice/handwritten-code/practice/singleton02.java
@@ -0,0 +1,11 @@
+public class Singleton2 {
+    private Singleton2() {}
+
+    private static class Holder {
+        private static final Singleton2 INSTANCE = new Singleton2();
+    }
+
+    public static Singleton2 getInstance() {
+        return Holder.INSTANCE;
+    }
+}
