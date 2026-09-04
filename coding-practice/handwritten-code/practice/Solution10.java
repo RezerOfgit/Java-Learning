e7809f650da214ddbf0e3c5bf7477ab1d7a18194
RezerOfgitee <qingcaicoke@163.com>
2026-09-04 17:58:15 +0800

    feat(leetcode): add solution for Regular Expression Matching (Problem #10)

---
+++ coding-practice/handwritten-code/practice/Solution10.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        // dp[i][j] 表示 s 的前 i 个字符与 p 的前 j 个字符是否匹配
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        // 处理 p 匹配空字符串的情况，如 "a*", "a*b*" 等
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (pc == '*') {
+                    // 情况1：匹配 0 次，跳过当前 '*' 和前一个字符
+                    dp[i][j] = dp[i][j - 2];
+                    // 情况2：匹配 1 次或多次，前提是前一个字符匹配
+                    char prev = p.charAt(j - 2);
+                    if (prev == '.' || prev == sc) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                } else {
+                    // 普通字符或 '.'，看当前字符是否匹配
+                    if (pc == '.' || pc == sc) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
